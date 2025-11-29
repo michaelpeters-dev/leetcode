e5fe3a2858e3cfd06ae5ea3fd6ead7898409a4ab
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2025-11-29 12:01:51 -0500

    1. Two Sum (Python)

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,22 +1,23 @@
 # Problem: Two Sum
 # Number: 1
+# Difficulty: Easy
 # URL: https://leetcode.com/problems/two-sum/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 12.93 MB
+# Memory: 13.15 MB
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        dictionary = {}
-        for index, num in enumerate(nums):
-            if target-num in dictionary:
-                return [dictionary[target-num], index]
-            else:
-                dictionary[num] = index
-   
-        
+class Solution(object):class Solution(object):
+    def twoSum(self, nums, target):    def twoSum(self, nums, target):
+        """        """
+        :type nums: List[int]        :type nums: List[int]
+        :type target: int        :type target: int
+        :rtype: List[int]        :rtype: List[int]
+        """        """
+        dictionary = {}        dictionary = {}
+        for index, num in enumerate(nums):        for index, num in enumerate(nums):
+            if target-num in dictionary:            if target-num in dictionary:
+                return [dictionary[target-num], index]                return [dictionary[target-num], index]
+            else:            else:
+                dictionary[num] = index                dictionary[num] = index
+      
+                
