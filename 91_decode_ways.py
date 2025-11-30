be1f9a14d479691d8e118417e9ccf087e112dec3
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2025-11-29 22:33:35 -0500

    91. Decode Ways (Python)

--- 91_decode_ways.py
+++ 91_decode_ways.py
@@ -4,24 +4,23 @@
 # URL: https://leetcode.com/problems/decode-ways/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 18.32 MB
+# Memory: 17.88 MB
+
+class Solution:class Solution:
+    def numDecodings(self, s: str) -> int:    def numDecodings(self, s: str) -> int:
+        dp = { len(s) : 1}        dp = { len(s) : 1}
 
         def dfs(i):        def dfs(i):
-                        
-            if i>=len(s):            if i>=len(s):
-                return 1                return 1
-            else:            else:
-            if (i+1)<=len(s)-1 and s[i:i+2] in mappings:            if (i+1)<=len(s)-1 and s[i:i+2] in mappings:
-            if s[i]=="0":            if s[i]=="0":
-                return 0                return 0
             if i in dp:            if i in dp:
                 return dp[i]                return dp[i]
-
-                return value                return value
-                value = dfs(i + 1) + dfs(i + 2)                value = dfs(i + 1) + dfs(i + 2)
-                dp[i] = value                dp[i] = value
-                return value                return value
-                value = dfs(i + 1)                value = dfs(i + 1)
-                dp[i] = value                dp[i] = value
+            if s[i] == "0":            if s[i] == "0":
+                return 0                return 0
+                        
+            res = dfs(i + 1)            res = dfs(i + 1)
+            if ((i + 1) < len(s)) and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456"):            if ((i + 1) < len(s)) and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456"):
+                res += dfs(i + 2)                res += dfs(i + 2)
+            return res            return res
+            dp[i] = res            dp[i] = res
                 
         return dfs(0)        return dfs(0)
+
