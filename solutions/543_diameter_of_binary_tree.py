8d37c3cd14333f6d456790781912feb448ac732a
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2025-12-07 02:18:14 -0500

    543. Diameter of Binary Tree (Python)

--- solutions/543_diameter_of_binary_tree.py
+++ solutions/543_diameter_of_binary_tree.py
@@ -6,22 +6,20 @@
 # Runtime: 0 ms
 # Memory: N/A
 
-        #                4     5        #                4     5
-        # max = 0        # max = 0
-        # 1 + max of both children each time        # 1 + max of both children each time
+#         self.val = val#         self.val = val
+#         self.left = left#         self.left = left
+#         self.right = right#         self.right = right
+class Solution:class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         maximum = [0]        maximum = [0]
         def dfs(root):        def dfs(root):
             if not root:            if not root:
                 return 0                return 0
-                        
             max_left_branch = dfs(root.left)            max_left_branch = dfs(root.left)
             max_right_branch = dfs(root.right)            max_right_branch = dfs(root.right)
-
-            maximum[0] = max(maximum[0], curr_max)            maximum[0] = max(maximum[0], curr_max)
+                        
+            curr_diameter = max_left_branch + max_right_branch            curr_diameter = max_left_branch + max_right_branch
+            maximum[0] = max(maximum[0], curr_diameter)            maximum[0] = max(maximum[0], curr_diameter)
             return 1 + max(max_left_branch, max_right_branch)            return 1 + max(max_left_branch, max_right_branch)
-            curr_max = max_left_branch + max_right_branch            curr_max = max_left_branch + max_right_branch
         dfs(root)        dfs(root)
         return maximum[0]        return maximum[0]
-                        
-                                        
-                
