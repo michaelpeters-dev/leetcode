6ed4aeeeaec31ede0f1a35780a92b4e38c380906
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2025-12-08 02:03:47 -0500

    4. Median of Two Sorted Arrays (Python)

--- solutions/4_median_of_two_sorted_arrays.py
+++ solutions/4_median_of_two_sorted_arrays.py
@@ -4,24 +4,21 @@
 # URL: https://leetcode.com/problems/median-of-two-sorted-arrays/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: N/A
+# Memory: 17.94 MB
 
-            j = half - i - 2 #B            j = half - i - 2 #B
+            ALeft = A[i] if i>=0 else float("-inf")            ALeft = A[i] if i>=0 else float("-inf")
+            ARight = A[i + 1] if i + 1 < len(A) else float("inf")            ARight = A[i + 1] if i + 1 < len(A) else float("inf")
+            BLeft = B[j] if j>=0 else float("-inf")            BLeft = B[j] if j>=0 else float("-inf")
+            BRight = B[j + 1] if j + 1 < len(B) else float("inf")            BRight = B[j + 1] if j + 1 < len(B) else float("inf")
 
-            Aleft = A[i] if i >= 0 else float("-inf")            Aleft = A[i] if i >= 0 else float("-inf")
-            Aright = A[i + 1] if (i+1) < len(A) else float("inf")            Aright = A[i + 1] if (i+1) < len(A) else float("inf")
-            Bleft = B[j] if j >= 0 else float("-inf")            Bleft = B[j] if j >= 0 else float("-inf")
-            Bright = B[j + 1] if (j + 1) < len(B) else float("inf")            Bright = B[j + 1] if (j + 1) < len(B) else float("inf")
+            if ALeft <= BRight and BLeft <= ARight:            if ALeft <= BRight and BLeft <= ARight:
+                if total%2==0:                if total%2==0:
+                    return (max(ALeft, BLeft) + min(ARight, BRight)) / 2                    return (max(ALeft, BLeft) + min(ARight, BRight)) / 2
+            # Calculate the lower and upper bounds of each array            # Calculate the lower and upper bounds of each array
 
-            if Aleft <= Bright and Bleft <= Aright:            if Aleft <= Bright and Bleft <= Aright:
-            # partition is correct            # partition is correct
-                # odd                # odd
-                if total % 2:                if total % 2:
-                    return min(Aright, Bright)                    return min(Aright, Bright)
-                else:                else:
-                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:            elif Aleft > Bright:
-                r = i - 1                r = i - 1
+                return min(ARight, BRight)                return min(ARight, BRight)
+            elif ALeft > BRight:            elif ALeft > BRight:
+                r -= 1                r -= 1
             else:            else:
-            i = (l + r) // 2 #A            i = (l + r) // 2 #A
-        while True:        while True:
+                l += 1                l += 1
+                        
