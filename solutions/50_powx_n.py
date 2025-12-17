dd8c7bf35ece2f26e1e6ec364376bfafe918ff42
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2025-12-17 09:46:14 +0200

    50. Pow(x, n) (Python)

--- solutions/50_powx_n.py
+++ solutions/50_powx_n.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/powx-n/
 # Submission Status: Accepted
-# Runtime: 2 ms
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 17.90 MB
 
 class Solution:class Solution:
     def myPow(self, x: float, n: int) -> float:    def myPow(self, x: float, n: int) -> float:
@@ -13,11 +13,11 @@ class Solution:class Solution:
                     return 0                    return 0
                 if n==0:                if n==0:
                     return 1                    return 1
-
-            res = helper(x, abs(n))            res = helper(x, abs(n))
                                 
                 res = helper(x, n//2)                res = helper(x, n//2)
 
                 res = res * res                res = res * res
                 return x * res if n%2!=0 else res                return x * res if n%2!=0 else res
+
+            res = helper(x, abs(n))            res = helper(x, abs(n))
             return res if n>=0 else 1/res            return res if n>=0 else 1/res
