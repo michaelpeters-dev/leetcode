1ac76662d1108066abf1dbe0257913c42626d7f7
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2025-12-24 08:55:10 +0200

    1046. Last Stone Weight (Python)

--- solutions/1046_last_stone_weight.py
+++ solutions/1046_last_stone_weight.py
@@ -3,19 +3,19 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/last-stone-weight/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: N/A
+# Runtime: 1 ms
+# Memory: 17.42 MB
 
-                        
-            y = arr[-1]            y = arr[-1]
-            x = arr[-2]            x = arr[-2]
+class Solution:class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-s for s in stones]        stones = [-s for s in stones]
+        heapq.heapify(stones)        heapq.heapify(stones)
 
-            if x==y:            if x==y:
-                arr.remove(x)                arr.remove(x)
-                arr.remove(y)                arr.remove(y)
-            else:            else:
-                return arr[0]                return arr[0]
-                arr.remove(x)                arr.remove(x)
-                arr[-1] = y - x                arr[-1] = y - x
-            return dfs(arr)            return dfs(arr)
-        return dfs(stones)        return dfs(stones)
+        while len(stones) > 1:        while len(stones) > 1:
+            first = heapq.heappop(stones)            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)            second = heapq.heappop(stones)
+            if second > first:            if second > first:
+                heapq.heappush(stones, first - second)                heapq.heappush(stones, first - second)
+        return abs(stones[0])        return abs(stones[0])
+        stones.append(0)        stones.append(0)
+                
