4a4e6c93629c43c2b090b18ad753b3a24300b272
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2025-12-25 08:40:06 +0200

    115. Distinct Subsequences (Python)

--- solutions/115_distinct_subsequences.py
+++ solutions/115_distinct_subsequences.py
@@ -3,21 +3,22 @@
 # Difficulty: Hard
 # URL: https://leetcode.com/problems/distinct-subsequences/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: N/A
+# Runtime: 224 ms
+# Memory: 17.26 MB
 
 class Solution:class Solution:
     def numDistinct(self, s: str, t: str) -> int:    def numDistinct(self, s: str, t: str) -> int:
         ROWS, COLS = len(s), len(t)        ROWS, COLS = len(s), len(t)
-        grid = [[0] * (COLS + 1) for _ in range(ROWS + 1)]        grid = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
-        for r in range(ROWS - 1, -1, -1):        for r in range(ROWS - 1, -1, -1):
-                    grid[r][c] = grid[r+1][c] + grid[r + 1][c + 1]                    grid[r][c] = grid[r+1][c] + grid[r + 1][c + 1]
-                else:                else:
-                    grid[r][c] = grid[r + 1][c]                    grid[r][c] = grid[r + 1][c]
-            for c in range(COLS - 1, -1, -1):            for c in range(COLS - 1, -1, -1):
-        grid[ROWS][COLS] = 1        grid[ROWS][COLS] = 1
-                if s[r]==t[c]:                if s[r]==t[c]:
-        for r in range(ROWS + 1 ):        for r in range(ROWS + 1 ):
-            grid[r][COLS] = 1            grid[r][COLS] = 1
 
-        return grid[0][0]        return grid[0][0]
+        dp = [0] * (COLS + 1)        dp = [0] * (COLS + 1)
+        dp[COLS] = 1        dp[COLS] = 1
+
+        for r in range(ROWS - 1, -1, -1):        for r in range(ROWS - 1, -1, -1):
+            prev_diag = 1 # grid[r+1][COLS] in 2D solution            prev_diag = 1 # grid[r+1][COLS] in 2D solution
+                        
+            for c in range(COLS - 1, -1, -1):            for c in range(COLS - 1, -1, -1):
+                temp = dp[c] # save grid[r + 1][c]                temp = dp[c] # save grid[r + 1][c]
+                if s[r]==t[c]:                if s[r]==t[c]:
+                    dp[c] += prev_diag                    dp[c] += prev_diag
+                prev_diag = temp                prev_diag = temp
+        return dp[0]        return dp[0]
