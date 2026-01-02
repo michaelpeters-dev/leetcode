572fee94b25e54b1e5604811578902c92db23d16
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-01-02 08:47:39 +0200

    678. Valid Parenthesis String (Python)

--- solutions/678_valid_parenthesis_string.py
+++ solutions/678_valid_parenthesis_string.py
@@ -3,21 +3,20 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/valid-parenthesis-string/
 # Submission Status: Accepted
-# Runtime: 9 ms
-# Memory: 18.08 MB
+# Runtime: 0 ms
+# Memory: 17.06 MB
 
-                        
-            if s[i]=="(":            if s[i]=="(":
-                res = dfs(i + 1, balance + 1)                res = dfs(i + 1, balance + 1)
-            if s[i]==")":            if s[i]==")":
-                res = dfs(i + 1, balance - 1)                res = dfs(i + 1, balance - 1)
-            if s[i]=="*":            if s[i]=="*":
-                res =  (                res =  (
-                    dfs(i + 1, balance) or                    dfs(i + 1, balance) or
-                    dfs(i + 1, balance + 1) or                    dfs(i + 1, balance + 1) or
-                    dfs(i + 1, balance - 1)                    dfs(i + 1, balance - 1)
-                )                )
-        return dfs(0, 0)        return dfs(0, 0)
-            memo[(i, balance)] = res            memo[(i, balance)] = res
-            return res            return res
 
+        for c in s:        for c in s:
+            if c=="(":            if c=="(":
+                leftMin, leftMax = leftMin  +1, leftMax + 1                leftMin, leftMax = leftMin  +1, leftMax + 1
+            elif c == ")":            elif c == ")":
+                leftMin, leftMax = leftMin  - 1, leftMax - 1                leftMin, leftMax = leftMin  - 1, leftMax - 1
+            else:            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1                leftMin, leftMax = leftMin - 1, leftMax + 1
+            if leftMax < 0:            if leftMax < 0:
+                return False                return False
+            if leftMin < 0:            if leftMin < 0:
+                leftMin = 0                leftMin = 0
+        leftMin, leftMax = 0, 0        leftMin, leftMax = 0, 0
+        return leftMin==0        return leftMin==0
