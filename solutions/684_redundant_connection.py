87fd435a7aaab185746826ff96a9ae505792e1eb
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-01-04 09:17:28 +0200

    684. Redundant Connection (Python)

--- solutions/684_redundant_connection.py
+++ solutions/684_redundant_connection.py
@@ -3,24 +3,23 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/redundant-connection/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: N/A
+# Runtime: 36 ms
+# Memory: 17.80 MB
+
+class Solution:class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        N = len(edges)        N = len(edges)
+        par = [i for i in range(N + 1)] #ith node ->. parent (1 - n)        par = [i for i in range(N + 1)] #ith node ->. parent (1 - n)
+        rank = [1] * (N + 1)        rank = [1] * (N + 1)
 
         def find(n):        def find(n):
+            if n != par[n]:            if n != par[n]:
+                par[n] = find(par[n])                par[n] = find(par[n])
+            return par[n]            return par[n]
+
         def union(n1, n2):        def union(n1, n2):
             p1, p2 = find(n1), find(n2)            p1, p2 = find(n1), find(n2)
             if p1 == p2:            if p1 == p2:
                 return False                return False
-            return True            return True
-
-            if n != par[n]:            if n != par[n]:
-                par[n] = find(par[n])                par[n] = find(par[n])
-
-            return par[n]            return par[n]
                         
             if rank[p1] > rank[p2]:            if rank[p1] > rank[p2]:
-                par[p2] = p1                par[p2] = p1
-            else:            else:
-                par[p1] = p2                par[p1] = p2
-                rank[p1] += rank[p2]                rank[p1] += rank[p2]
-                rank[p2] += rank[p1]                rank[p2] += rank[p1]
