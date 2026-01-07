1ccf3ef3557f5e4752518a82a7b79cc781b499f5
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-01-07 08:31:51 +0200

    746. Min Cost Climbing Stairs (Python)

--- solutions/746_min_cost_climbing_stairs.py
+++ solutions/746_min_cost_climbing_stairs.py
@@ -3,19 +3,13 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/min-cost-climbing-stairs/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: N/A
+# Runtime: 4 ms
+# Memory: 19.44 MB
 
 class Solution:class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = {}        memo = {}
-        def dfs(i):        def dfs(i):
-            if i>=len(cost):            if i>=len(cost):
-                return 0                return 0
-            minimum = cost[i] + min(dfs(i + 1), dfs(i + 2))            minimum = cost[i] + min(dfs(i + 1), dfs(i + 2))
-            return memo[i]            return memo[i]
-        res = min(dfs(0), dfs(1))        res = min(dfs(0), dfs(1))
-            if i in memo:            if i in memo:
-                return memo[i]                return memo[i]
-            memo[i] = minimum            memo[i] = minimum
-        return res        return res
+        dp = [0] * (len(cost) + 2)        dp = [0] * (len(cost) + 2)
+            dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])            dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])
+        return min(dp[0], dp[1])        return min(dp[0], dp[1])
+        for i in range(len(cost) - 1, -1, -1):        for i in range(len(cost) - 1, -1, -1):
+
