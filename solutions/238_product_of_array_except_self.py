97d1dbc7755835832db838b4ce73edb8bb5a52c9
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-02-24 23:08:58 -0500

    238. Product of Array Except Self (Python)

--- solutions/238_product_of_array_except_self.py
+++ solutions/238_product_of_array_except_self.py
@@ -3,22 +3,18 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/product-of-array-except-self/
 # Submission Status: Accepted
-# Runtime: 23 ms
-# Memory: 25.68 MB
+# Runtime: 0 ms
+# Memory: N/A
 
-    def productExceptSelf(self, nums: List[int]) -> List[int]:    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prevL = 1        prevL = 1
-        prevR = 1        prevR = 1
-        res = [1] * len(nums)        res = [1] * len(nums)
+        for (auto num: backward) {        for (auto num: backward) {
+            cout << num << " ";            cout << num << " ";
+        }        }
 
-        L = 0        L = 0
-        R = len(nums) - 1        R = len(nums) - 1
-        while L<len(nums):        while L<len(nums):
-            res[L] *= prevL            res[L] *= prevL
-            res[R] *= prevR            res[R] *= prevR
-            prevL = nums[L] * prevL            prevL = nums[L] * prevL
-            prevR = nums[R] * prevR            prevR = nums[R] * prevR
-                        
-            L += 1            L += 1
-            R -= 1            R -= 1
-        return res        return res
+        vector<int> ans(nums.size());        vector<int> ans(nums.size());
+        for (int i = 0; i < nums.size(); i++) {        for (int i = 0; i < nums.size(); i++) {
+            ans[i] = forward[i] * backward[i];            ans[i] = forward[i] * backward[i];
+        }        }
+        return ans;        return ans;
+    }    }
+                
+};};
