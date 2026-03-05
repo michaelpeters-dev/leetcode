2d588b7ad5fcf85f84ee09f70ae9e3baf1789c86
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-05 17:42:31 -0500

    242. Valid Anagram (Python)

--- solutions/242_valid_anagram.py
+++ solutions/242_valid_anagram.py
@@ -3,21 +3,20 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/valid-anagram/
 # Submission Status: Accepted
-# Runtime: 2 ms
-# Memory: 9.89 MB
+# Runtime: 0 ms
+# Memory: 9.72 MB
 
-        unordered_map<char, int> second;        unordered_map<char, int> second;
-
-        for (int i = 0; i < n; i++) {        for (int i = 0; i < n; i++) {
-        int n = s.size();        int n = s.size();
-            first[s[i]] += 1;            first[s[i]] += 1;
-        }        }
-            second[t[i]] += 1;            second[t[i]] += 1;
-        unordered_map<char, int> first;        unordered_map<char, int> first;
-
-        }        }
-    bool isAnagram(string s, string t) {    bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {        if (s.size() != t.size()) {
             return false;            return false;
-public:public:
-class Solution {class Solution {
+        }        }
+
+        unordered_map<char, int> smap;        unordered_map<char, int> smap;
+
+        unordered_map<char, int> tmap;        unordered_map<char, int> tmap;
+        for (int i = 0; i < s.size(); i++) {        for (int i = 0; i < s.size(); i++) {
+            smap[s[i]] += 1;                   smap[s[i]] += 1;       
+        }        }
+            tmap[t[i]] += 1;                   tmap[t[i]] += 1;       
+
+    }    }
+        return (smap == tmap);        return (smap == tmap);
+};};
