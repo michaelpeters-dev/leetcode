dad977c148028c3ebaea691eb86100aa1c22d19f
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-05 17:45:54 -0500

    1. Two Sum (Python)

--- solutions/1_two_sum.py
+++ solutions/1_two_sum.py
@@ -3,21 +3,20 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/two-sum/
 # Submission Status: Accepted
-# Runtime: 3 ms
+# Runtime: 0 ms
 # Memory: 14.74 MB
 
 class Solution {class Solution {
 public:public:
     vector<int> twoSum(vector<int>& nums, int target) {    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> store;        unordered_map<int, int> store;
+
         for (int i = 0; i < nums.size(); i++) {        for (int i = 0; i < nums.size(); i++) {
-            int need = target - nums[i];            int need = target - nums[i];
-            if (store.count(need)) {            if (store.count(need)) {
+            int offset = target - nums[i];            int offset = target - nums[i];
         }        }
-                return {store[need], i};                return {store[need], i};
-            } else {            } else {
-                store[nums[i]] = i;                store[nums[i]] = i;
+            if (store.count(offset)) {            if (store.count(offset)) {
+                return {store[offset], i};                return {store[offset], i};
             }            }
     }    }
-        return {-1};        return {-1};
-};};
+            store[nums[i]] = i;            store[nums[i]] = i;
+        return {};        return {};
