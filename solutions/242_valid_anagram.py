f541e3e43c745f136bdaa2448125c89cfd50599c
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-13 20:21:53 -0400

    242. Valid Anagram (Python)

--- solutions/242_valid_anagram.py
+++ solutions/242_valid_anagram.py
@@ -3,20 +3,24 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/valid-anagram/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: 9.72 MB
+# Runtime: 7 ms
+# Memory: 14.18 MB
 
-            return false;            return false;
-        }        }
-
-        unordered_map<char, int> smap;        unordered_map<char, int> smap;
-
-        unordered_map<char, int> tmap;        unordered_map<char, int> tmap;
-        for (int i = 0; i < s.size(); i++) {        for (int i = 0; i < s.size(); i++) {
-            smap[s[i]] += 1;                   smap[s[i]] += 1;       
-        }        }
-            tmap[t[i]] += 1;                   tmap[t[i]] += 1;       
-
-    }    }
-        return (smap == tmap);        return (smap == tmap);
-};};
+from collections import defaultdictfrom collections import defaultdict
+    def isAnagram(self, s, t):    def isAnagram(self, s, t):
+        """        """
+        :type s: str        :type s: str
+        :type t: str        :type t: str
+        :rtype: bool        :rtype: bool
+        """        """
+        set_a = defaultdict(int)        set_a = defaultdict(int)
+        set_b = defaultdict(int)        set_b = defaultdict(int)
+        if len(s) != len(t):        if len(s) != len(t):
+            return False            return False
+                
+        for i in range(0, len(s)):        for i in range(0, len(s)):
+class Solution(object):class Solution(object):
+            set_a[s[i]] += 1            set_a[s[i]] += 1
+            set_b[t[i]] += 1            set_b[t[i]] += 1
+                
+        return set_a == set_b        return set_a == set_b
