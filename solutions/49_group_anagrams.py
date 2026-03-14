1b22457639dd951e3a1064613ddd9b906b25bcf3
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-13 20:30:35 -0400

    49. Group Anagrams (Python)

--- solutions/49_group_anagrams.py
+++ solutions/49_group_anagrams.py
@@ -3,26 +3,26 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/group-anagrams/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 19 ms
+# Memory: 16.74 MB
 
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        anagrams = {}
-        for string in strs:
-            sorted_str = sorted(string)
-            temp = ""
-            for letter in sorted_str:
-                temp += letter
-            if temp in anagrams:
-                anagrams[temp].append(string)
-            else:
-                anagrams[temp] = [string]
-        results = []
-        for value in anagrams.values():
-            results.append(value)
-        return results
+class Solution(object):class Solution(object):
+    def groupAnagrams(self, strs):    def groupAnagrams(self, strs):
+        """        """
+        :type strs: List[str]        :type strs: List[str]
+        :rtype: List[List[str]]        :rtype: List[List[str]]
+        """        """
+        result = {}        result = {}
+
+        for string in strs:        for string in strs:
+            temp = "".join(sorted(string))            temp = "".join(sorted(string))
+            if temp in result:            if temp in result:
+                result[temp].append(string)                result[temp].append(string)
+            else:            else:
+                result[temp] = [string]                result[temp] = [string]
+                
+        return_list = []        return_list = []
+        for key, value in result.items():        for key, value in result.items():
+            return_list.append(value)            return_list.append(value)
+        return return_list        return return_list
+                                
