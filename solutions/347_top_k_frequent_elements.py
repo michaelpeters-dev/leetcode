713a2bdcfea180fceeca139d647c8f6b70e5910f
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-13 20:42:24 -0400

    347. Top K Frequent Elements (Python)

--- solutions/347_top_k_frequent_elements.py
+++ solutions/347_top_k_frequent_elements.py
@@ -4,21 +4,22 @@
 # URL: https://leetcode.com/problems/top-k-frequent-elements/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 21.00 MB
+# Memory: N/A
 
-class·‌Solution:class·‌Solution:
-·‌·‌·‌·‌def·‌topKFrequent(self,·‌nums:·‌List[int],·‌k:·‌int)·‌->·‌List[int]:·‌·‌·‌·‌def·‌topKFrequent(self,·‌nums:·‌List[int],·‌k:·‌int)·‌->·‌List[int]:
-·‌·‌·‌·‌·‌·‌·‌·‌count·‌=·‌{}·‌·‌·‌·‌·‌·‌·‌·‌count·‌=·‌{}
-·‌·‌·‌·‌·‌·‌·‌·‌freq·‌=·‌[[]·‌for·‌i·‌in·‌range(len(nums)·‌+·‌1)]·‌·‌·‌·‌·‌·‌·‌·‌freq·‌=·‌[[]·‌for·‌i·‌in·‌range(len(nums)·‌+·‌1)]
+class Solution(object):class Solution(object):
+    def topKFrequent(self, nums, k):    def topKFrequent(self, nums, k):
+        """        """
+        :type nums: List[int]        :type nums: List[int]
+        :type k: int        :type k: int
+        :rtype: List[int]        :rtype: List[int]
+        """        """
+        store = {}        store = {}
+        for num in nums:        for num in nums:
+            if num not in store:            if num not in store:
+                store[num] = 1                store[num] = 1
+            else:            else:
+                store[num] += 1                store[num] += 1
+                
+        sorted_nums = sorted(store, key=store.get, reverse=True)        sorted_nums = sorted(store, key=store.get, reverse=True)
 
-·‌·‌·‌·‌·‌·‌·‌·‌for·‌n·‌in·‌nums:·‌·‌·‌·‌·‌·‌·‌·‌for·‌n·‌in·‌nums:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌count[n]·‌=·‌1·‌+·‌count.get(n,·‌0)·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌count[n]·‌=·‌1·‌+·‌count.get(n,·‌0)
-·‌·‌·‌·‌·‌·‌·‌·‌for·‌n,·‌c·‌in·‌count.items():·‌·‌·‌·‌·‌·‌·‌·‌for·‌n,·‌c·‌in·‌count.items():
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌freq[c].append(n)·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌freq[c].append(n)
-
-·‌·‌·‌·‌·‌·‌·‌·‌res·‌=·‌[]·‌·‌·‌·‌·‌·‌·‌·‌res·‌=·‌[]
-·‌·‌·‌·‌·‌·‌·‌·‌for·‌i·‌in·‌range(len(freq)·‌-·‌1,·‌0,·‌-1):·‌·‌·‌·‌·‌·‌·‌·‌for·‌i·‌in·‌range(len(freq)·‌-·‌1,·‌0,·‌-1):
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌for·‌n·‌in·‌freq[i]:·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌for·‌n·‌in·‌freq[i]:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌res.append(n)·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌res.append(n)
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌if·‌len(res)==k:·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌if·‌len(res)==k:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌return·‌res·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌return·‌res
+        return sorted_nums[:k]        return sorted_nums[:k]
