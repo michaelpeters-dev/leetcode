fcb63ccbd62c6f9fecd0a07ebe52b9b9e80f6b18
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-13 20:23:36 -0400

    242. Valid Anagram (Python)

--- solutions/242_valid_anagram.py
+++ solutions/242_valid_anagram.py
@@ -3,24 +3,27 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/valid-anagram/
 # Submission Status: Accepted
-# Runtime: 7 ms
-# Memory: 14.18 MB
+# Runtime: 11 ms
+# Memory: 14.35 MB
 
 from collections import defaultdictfrom collections import defaultdict
+class Solution(object):class Solution(object):
     def isAnagram(self, s, t):    def isAnagram(self, s, t):
         """        """
         :type s: str        :type s: str
         :type t: str        :type t: str
         :rtype: bool        :rtype: bool
         """        """
-        set_a = defaultdict(int)        set_a = defaultdict(int)
-        set_b = defaultdict(int)        set_b = defaultdict(int)
         if len(s) != len(t):        if len(s) != len(t):
             return False            return False
-                
-        for i in range(0, len(s)):        for i in range(0, len(s)):
-class Solution(object):class Solution(object):
+
+        set_a = defaultdict(int)        set_a = defaultdict(int)
+        set_b = defaultdict(int)        set_b = defaultdict(int)
+
+        for i in range(len(s)):        for i in range(len(s)):
+
             set_a[s[i]] += 1            set_a[s[i]] += 1
             set_b[t[i]] += 1            set_b[t[i]] += 1
-                
+
         return set_a == set_b        return set_a == set_b
+                
