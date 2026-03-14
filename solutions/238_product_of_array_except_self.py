f57f3acbc047ff0da5d10f6272107143267bb16a
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-14 14:25:01 -0400

    238. Product of Array Except Self (Python)

--- solutions/238_product_of_array_except_self.py
+++ solutions/238_product_of_array_except_self.py
@@ -3,18 +3,26 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/product-of-array-except-self/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: N/A
+# Runtime: 32 ms
+# Memory: 20.19 MB
 
-        for (auto num: backward) {        for (auto num: backward) {
-            cout << num << " ";            cout << num << " ";
-        }        }
+class Solution(object):class Solution(object):
+    def productExceptSelf(self, nums):    def productExceptSelf(self, nums):
+        """        """
+        :type nums: List[int]        :type nums: List[int]
+        :rtype: List[int]        :rtype: List[int]
+        """        """
+        n = len(nums)        n = len(nums)
+        result = [1] * n        result = [1] * n
 
-        vector<int> ans(nums.size());        vector<int> ans(nums.size());
-        for (int i = 0; i < nums.size(); i++) {        for (int i = 0; i < nums.size(); i++) {
-            ans[i] = forward[i] * backward[i];            ans[i] = forward[i] * backward[i];
-        }        }
-        return ans;        return ans;
-    }    }
+        prefix = 1        prefix = 1
+        for i in range(n):        for i in range(n):
+            result[i] = prefix            result[i] = prefix
+            prefix *= nums[i]            prefix *= nums[i]
                 
-};};
+        suffix = 1        suffix = 1
+        for i in range(n-1, -1, -1):        for i in range(n-1, -1, -1):
+            result[i] *= suffix            result[i] *= suffix
+            suffix *= nums[i]            suffix *= nums[i]
+        return result        return result
+                
