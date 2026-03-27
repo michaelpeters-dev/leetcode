0a807f84667ceffea7c1824b9b49aa6015a553b3
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-26 20:48:58 -0500

    167. Two Sum II - Input Array Is Sorted (Python)

--- solutions/167_two_sum_ii_input_array_is_sorted.py
+++ solutions/167_two_sum_ii_input_array_is_sorted.py
@@ -3,17 +3,26 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 # Submission Status: Accepted
-# Runtime: 7 ms
-# Memory: 18.59 MB
+# Runtime: 0 ms
+# Memory: 19.35 MB
 
-class Solution:class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers)-1        l, r = 0, len(numbers)-1
-        while l<=r:        while l<=r:
-            if numbers[l] + numbers[r] == target:            if numbers[l] + numbers[r] == target:
-                return [l + 1, r + 1]                return [l + 1, r + 1]
-            elif numbers[l] + numbers[r] > target:            elif numbers[l] + numbers[r] > target:
-                r -= 1                r -= 1
-            else:            else:
-                l += 1                l += 1
-        return -1        return -1
+class Solution {class Solution {
+public:public:
+    vector<int> twoSum(vector<int>& numbers, int target) {    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();        int n = numbers.size();
+
+            int sum = numbers[l] + numbers[r];            int sum = numbers[l] + numbers[r];
+        int l = 0;        int l = 0;
+        int r = n - 1;        int r = n - 1;
+
+        for (int i = 0; i < n - 1; i++){        for (int i = 0; i < n - 1; i++){
+        }        }
+            if (sum == target) {            if (sum == target) {
+                return {l + 1, r + 1};                return {l + 1, r + 1};
+            }            }
+    }    }
+
+            if (sum > target) r--;            if (sum > target) r--;
+            if (sum < target) l++;            if (sum < target) l++;
+        return {0};        return {0};
+};};
