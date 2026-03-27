87ee079cdf47e3a9f1af6ce74bd3aa1c088b2ae1
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-03-27 10:24:00 -0500

    11. Container With Most Water (Python)

--- solutions/11_container_with_most_water.py
+++ solutions/11_container_with_most_water.py
@@ -3,19 +3,30 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/container-with-most-water/
 # Submission Status: Accepted
-# Runtime: N/A
+# Runtime: 0 ms
 # Memory: N/A
 
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height)-1
-        res = 0
+class Solution {class Solution {
+public:public:
+    int maxArea(vector<int>& height) {    int maxArea(vector<int>& height) {
+        int n = height.size();        int n = height.size();
+        int l = 0;        int l = 0;
+        int r = n - 1;        int r = n - 1;
 
-        while l < r:
-            res = max(res, (r - l) * min(height[l], height[r]))
-            if height[l] < height[r]:
-                l += 1
-            else:
-                r -= 1
+        while (l < r) {        while (l < r) {
+            int x = r - l;            int x = r - l;
+            int y = min(height[l], height[r]);            int y = min(height[l], height[r]);
+            int area = x * y;            int area = x * y;
+            result = max(result, area);            result = max(result, area);
+        int result = INT_MIN;        int result = INT_MIN;
+        return result;        return result;
+        }        }
 
-        return res
+
+            if (height[l]<height[r]) {            if (height[l]<height[r]) {
+                l++;                l++;
+            } else {            } else {
+                r--;                r--;
+            }            }
+    }    }
+};};
