b9f2c2e6875ed14f71657ddaf40d8c69a85c3081
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-02 15:28:12 -0400

    49. Group Anagrams (Python)

--- solutions/49_group_anagrams.py
+++ solutions/49_group_anagrams.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/group-anagrams/
 # Submission Status: Accepted
-# Runtime: 19 ms
-# Memory: 16.74 MB
+# Runtime: 0 ms
+# Memory: N/A
 
 class Solution(object):class Solution(object):
     def groupAnagrams(self, strs):    def groupAnagrams(self, strs):
@@ -12,17 +12,17 @@ class Solution(object):class Solution(object):
         :type strs: List[str]        :type strs: List[str]
         :rtype: List[List[str]]        :rtype: List[List[str]]
         """        """
-        result = {}        result = {}
 
+        store = {}        store = {}
         for string in strs:        for string in strs:
             temp = "".join(sorted(string))            temp = "".join(sorted(string))
-            if temp in result:            if temp in result:
-                result[temp].append(string)                result[temp].append(string)
+            if temp in store:            if temp in store:
+                store[temp].append(string)                store[temp].append(string)
             else:            else:
-                result[temp] = [string]                result[temp] = [string]
+                store[temp] = []                store[temp] = []
+        result = []        result = []
+        for key, value in store.items():        for key, value in store.items():
+            result.append(value)            result.append(value)
                 
-        return_list = []        return_list = []
-        for key, value in result.items():        for key, value in result.items():
-            return_list.append(value)            return_list.append(value)
-        return return_list        return return_list
-                                
+                store[temp].append(string)                store[temp].append(string)
+        return result        return result
