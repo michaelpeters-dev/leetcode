422d0a2826a902c58c7964c7a7e4fec536762c12
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-25 19:54:01 -0400

    217. Contains Duplicate (Python)

--- solutions/217_contains_duplicate.py
+++ solutions/217_contains_duplicate.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/contains-duplicate/
 # Submission Status: Accepted
-# Runtime: 19 ms
-# Memory: 25.95 MB
+# Runtime: 24 ms
+# Memory: 26.00 MB
 
 class Solution(object):class Solution(object):
     def containsDuplicate(self, nums):    def containsDuplicate(self, nums):
@@ -15,9 +15,9 @@ class Solution(object):class Solution(object):
         store = set()        store = set()
 
         for num in nums:        for num in nums:
-            if num in store:            if num in store:
-                return True                 return True 
-            else:            else:
+            if num not in store:            if num not in store:
                 store.add(num)                store.add(num)
-        return False         return False 
+            else:            else:
+                return True                return True
+        return False        return False
                 
