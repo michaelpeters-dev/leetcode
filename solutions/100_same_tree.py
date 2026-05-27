88e336ab46551de07e7cb1c0369e64cb456d2c55
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-26 20:28:39 -0400

    100. Same Tree (Python)

--- solutions/100_same_tree.py
+++ solutions/100_same_tree.py
@@ -3,15 +3,21 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/same-tree/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-
-        if not p or not q or p.val!=q.val:
-            return False
-
-        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        :rtype: bool        :rtype: bool
+        """        """
+        def helper(p, q):        def helper(p, q):
+            if (not p and not q):            if (not p and not q):
+                        
+            return helper(p.left, q.left) and helper(p.right, q.right)            return helper(p.left, q.left) and helper(p.right, q.right)
+            if (not p and q) or (p and not q):            if (not p and q) or (p and not q):
+                return False                 return False 
+                return True                return True
+        :type q: Optional[TreeNode]        :type q: Optional[TreeNode]
+        :type p: Optional[TreeNode]        :type p: Optional[TreeNode]
+            if p.val != q.val:            if p.val != q.val:
+                return False                return False
+                
+        return helper(p, q)        return helper(p, q)
