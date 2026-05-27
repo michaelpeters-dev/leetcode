7293d3ee0d9312860b844e70b318e867358bae69
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-26 20:21:03 -0400

    347. Top K Frequent Elements (Python)

--- solutions/347_top_k_frequent_elements.py
+++ solutions/347_top_k_frequent_elements.py
@@ -4,21 +4,18 @@
 # URL: https://leetcode.com/problems/top-k-frequent-elements/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 0.00 MB
+# Memory: 21.79 MB
 
                 
-        freq_map = {i:[] for i in range(0, len(nums) + 1)}        freq_map = {i:[] for i in range(0, len(nums) + 1)}
+        freq = {i:[] for i in range(len(nums) + 1)}        freq = {i:[] for i in range(len(nums) + 1)}
 
         for key, value in counter.items():        for key, value in counter.items():
-            freq_map[value].append(key)            freq_map[value].append(key)
+            freq[value].append(key)            freq[value].append(key)
                 
         result = []        result = []
 
-        for freq in range(len(freq_map)-1, -1, -1):        for freq in range(len(freq_map)-1, -1, -1):
-            for num in freq_map[freq]:            for num in freq_map[freq]:
+        for f in range(len(freq) - 1, -1, -1):        for f in range(len(freq) - 1, -1, -1):
+            for num in freq[f]:            for num in freq[f]:
                 result.append(num)                result.append(num)
-            counter[num] = counter[num] + 1            counter[num] = counter[num] + 1
-        for num in nums:        for num in nums:
-
                 if len(result) == k:                if len(result) == k:
                     return result                    return result
