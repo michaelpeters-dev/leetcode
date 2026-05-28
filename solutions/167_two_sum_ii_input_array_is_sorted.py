78108ef0d357fad14312965f2ed6535aaa14c0f9
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-27 21:23:39 -0400

    167. Two Sum II - Input Array Is Sorted (Python)

--- solutions/167_two_sum_ii_input_array_is_sorted.py
+++ solutions/167_two_sum_ii_input_array_is_sorted.py
@@ -4,25 +4,23 @@
 # URL: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 19.35 MB
+# Memory: 0.00 MB
 
-class Solution {class Solution {
-public:public:
-    vector<int> twoSum(vector<int>& numbers, int target) {    vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();        int n = numbers.size();
+class Solution(object):class Solution(object):
+    def twoSum(self, numbers, target):    def twoSum(self, numbers, target):
+        """        """
+        :type numbers: List[int]        :type numbers: List[int]
+        :type target: int        :type target: int
+        :rtype: List[int]        :rtype: List[int]
+        """        """
+        r = len(numbers) - 1        r = len(numbers) - 1
+            if (total == target):            if (total == target):
 
-            int sum = numbers[l] + numbers[r];            int sum = numbers[l] + numbers[r];
-        int l = 0;        int l = 0;
-        int r = n - 1;        int r = n - 1;
-
-        for (int i = 0; i < n - 1; i++){        for (int i = 0; i < n - 1; i++){
-        }        }
-            if (sum == target) {            if (sum == target) {
-                return {l + 1, r + 1};                return {l + 1, r + 1};
-            }            }
-    }    }
-
-            if (sum > target) r--;            if (sum > target) r--;
-            if (sum < target) l++;            if (sum < target) l++;
-        return {0};        return {0};
-};};
+                return [l, r]                return [l, r]
+        while l<r:        while l<r:
+            total = numbers[l] + numbers[r]            total = numbers[l] + numbers[r]
+            elif (total < target):            elif (total < target):
+                l += 1                l += 1
+            else:            else:
+        l = 0        l = 0
+                r -= 1                r -= 1
