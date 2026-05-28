c9842c082cca5a3f5bd4613f27cdc7dbd6afba8b
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-27 21:15:33 -0400

    128. Longest Consecutive Sequence (Python)

--- solutions/128_longest_consecutive_sequence.py
+++ solutions/128_longest_consecutive_sequence.py
@@ -3,27 +3,31 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/longest-consecutive-sequence/
 # Submission Status: Accepted
-# Runtime: 86 ms
-# Memory: 26.80 MB
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
+class Solution(object):class Solution(object):
+    def longestConsecutive(self, nums):    def longestConsecutive(self, nums):
         """        """
         :type nums: List[int]        :type nums: List[int]
         :rtype: int        :rtype: int
         """        """
-        store = set()        store = set()
 
+        # Add them all to a set        # Add them all to a set
+        # Then iterate through, if we don't have a number - 1 in the set then that's where we count from        # Then iterate through, if we don't have a number - 1 in the set then that's where we count from
+
+        store = set()        store = set()
         for num in nums:        for num in nums:
             store.add(num)            store.add(num)
-                
+
         for num in store:        for num in store:
-            if num-1 in store:            if num-1 in store:
-                continue                continue
-            else:            else:
-        largest = 0        largest = 0
-            temp = 0            temp = 0
-                while num in store:                while num in store:
-                    temp += 1                    temp += 1
+        longest = 0        longest = 0
+            if (num - 1) not in store:            if (num - 1) not in store:
+                counter = 1                counter = 1
+                while (num + 1) in store:                while (num + 1) in store:
+                    counter += 1                    counter += 1
                     num += 1                    num += 1
-                    largest = max(largest, temp)                    largest = max(largest, temp)
-        return largest        return largest
-                
+                longest = max(longest, counter)                longest = max(longest, counter)
+            else:            else:
+                continue                continue
+        return longest        return longest
