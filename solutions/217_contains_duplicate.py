afbc523e22f5557445f3b221a715397efcb5e681
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-29 16:35:19 -0400

    217. Contains Duplicate (Python)

--- solutions/217_contains_duplicate.py
+++ solutions/217_contains_duplicate.py
@@ -3,21 +3,21 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/contains-duplicate/
 # Submission Status: Accepted
-# Runtime: 24 ms
-# Memory: 26.00 MB
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution(object):class Solution(object):
-    def containsDuplicate(self, nums):    def containsDuplicate(self, nums):
-        """        """
-        :type nums: List[int]        :type nums: List[int]
-        :rtype: bool        :rtype: bool
-        """        """
-        store = set()        store = set()
+class Solution {class Solution {
+public:public:
+    bool containsDuplicate(vector<int>& nums) {    bool containsDuplicate(vector<int>& nums) {
+        for (auto& num: nums) {        for (auto& num: nums) {
+        }        }
 
-        for num in nums:        for num in nums:
-            if num not in store:            if num not in store:
-                store.add(num)                store.add(num)
-            else:            else:
-                return True                return True
-        return False        return False
-                
+        return false;        return false;
+        unordered_set<int> store;        unordered_set<int> store;
+            if (store.count(num)) {            if (store.count(num)) {
+                return true;                return true;
+            } else {            } else {
+                store.insert(num);                store.insert(num);
+            }            }
+    }    }
+};};
