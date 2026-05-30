0c619855a7047b374eeef9a28102597ba6cd3d30
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-29 23:33:23 -0400

    347. Top K Frequent Elements (Python)

--- solutions/347_top_k_frequent_elements.py
+++ solutions/347_top_k_frequent_elements.py
@@ -4,18 +4,33 @@
 # URL: https://leetcode.com/problems/top-k-frequent-elements/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 21.79 MB
+# Memory: N/A
 
-                
-        freq = {i:[] for i in range(len(nums) + 1)}        freq = {i:[] for i in range(len(nums) + 1)}
+        unordered_map<int, vector<int>> mappings; // Storing in the form: {Frequency, number}        unordered_map<int, vector<int>> mappings; // Storing in the form: {Frequency, number}
+        for (int i = 0; i < nums.size(); i++) {        for (int i = 0; i < nums.size(); i++) {
+            mappings[i] = {};            mappings[i] = {};
+        }        }
 
-        for key, value in counter.items():        for key, value in counter.items():
-            freq[value].append(key)            freq[value].append(key)
-                
-        result = []        result = []
+        // Looping through the counter numbers and reading them into the mappings        // Looping through the counter numbers and reading them into the mappings
+        for (auto& num: counter) {        for (auto& num: counter) {
+            mappings[num.second].push_back(num.first);            mappings[num.second].push_back(num.first);
+        }        }
 
-        for f in range(len(freq) - 1, -1, -1):        for f in range(len(freq) - 1, -1, -1):
-            for num in freq[f]:            for num in freq[f]:
-                result.append(num)                result.append(num)
-                if len(result) == k:                if len(result) == k:
-                    return result                    return result
+        for (int i = nums.size(); i>0; i--) {        for (int i = nums.size(); i>0; i--) {
+        // Looping in descending order and reading the result in        // Looping in descending order and reading the result in
+        vector<int> result;        vector<int> result;
+            for (auto& num: mappings[i]) {            for (auto& num: mappings[i]) {
+        }        }
+                result.push_back(num);                result.push_back(num);
+            }            }
+                if (result.size() == k) {                if (result.size() == k) {
+                    return result;                    return result;
+                }                }
+
+        }        }
+            counter[num]++;            counter[num]++;
+        for (auto& num: nums) {        for (auto& num: nums) {
+        unordered_map<int, int> counter; // Storing in the form: {Number: Count}        unordered_map<int, int> counter; // Storing in the form: {Number: Count}
+    vector<int> topKFrequent(vector<int>& nums, int k) {    vector<int> topKFrequent(vector<int>& nums, int k) {
+public:public:
+class Solution {class Solution {
