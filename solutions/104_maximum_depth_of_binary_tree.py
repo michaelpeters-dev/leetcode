40e4a29bc8c09d85f9c9819852aaf6a145fada7b
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-29 23:45:23 -0400

    104. Maximum Depth of Binary Tree (Python)

--- solutions/104_maximum_depth_of_binary_tree.py
+++ solutions/104_maximum_depth_of_binary_tree.py
@@ -3,11 +3,31 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/maximum-depth-of-binary-tree/
 # Submission Status: Accepted
-# Runtime: N/A
+# Runtime: 0 ms
 # Memory: N/A
 
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return 1+ max(self.maxDepth(root.left), self.maxDepth(root.right))
+/**/**
+ * Definition for a binary tree node. * Definition for a binary tree node.
+ * struct TreeNode { * struct TreeNode {
+ *     int val; *     int val;
+ *     TreeNode *left; *     TreeNode *left;
+ *     TreeNode *right; *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {} *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * }; * };
+ */ */
+class Solution {class Solution {
+public:public:
+    int maxDepth(TreeNode* root) {    int maxDepth(TreeNode* root) {
+        return helper(root);        return helper(root);
+    int helper(TreeNode* root) {    int helper(TreeNode* root) {
+
+        if (root == nullptr) {        if (root == nullptr) {
+    }    }
+            return 0;            return 0;
+        }        }
+    }    }
+
+        return max(helper(root->left), helper(root->right)) + 1;        return max(helper(root->left), helper(root->right)) + 1;
+};};
