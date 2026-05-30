e8a269bce42b6e8d7ab52e8e5b79f1470b8f6b35
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-29 23:09:31 -0400

    1. Two Sum (Python)

--- solutions/1_two_sum.py
+++ solutions/1_two_sum.py
@@ -4,21 +4,19 @@
 # URL: https://leetcode.com/problems/two-sum/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 13.00 MB
+# Memory: 0.00 MB
 
-class Solution(object):class Solution(object):
-    def twoSum(self, nums, target):    def twoSum(self, nums, target):
-        """        """
-        :type nums: List[int]        :type nums: List[int]
-        :type target: int        :type target: int
-        :rtype: List[int]        :rtype: List[int]
-        """        """
-        store = {}        store = {}
-
-        for index, num in enumerate(nums):        for index, num in enumerate(nums):
-            if (target - num) in store:            if (target - num) in store:
-                return [store[target - num], index]                return [store[target - num], index]
-            else:            else:
-                store[num] = index                store[num] = index
-        return []        return []
-                
+class Solution {class Solution {
+public:public:
+    vector<int> twoSum(vector<int>& nums, int target) {    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> store;        unordered_map<int, int> store;
+        for (int i = 0; i < nums.size(); i++) {        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];            int num = nums[i];
+            if (store.count(target - num)) {            if (store.count(target - num)) {
+                return {store[target - num], i};                return {store[target - num], i};
+            }            }
+        }        }
+        return {};        return {};
+    }    }
+            store[nums[i]] = i;            store[nums[i]] = i;
+};};
