5e6374c31a53ed9b0d1febb6a0e8b0e3f6ad4770
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-30 21:57:55 -0400

    42. Trapping Rain Water (Python)

--- solutions/42_trapping_rain_water.py
+++ solutions/42_trapping_rain_water.py
@@ -6,27 +6,24 @@
 # Runtime: 0 ms
 # Memory: N/A
 
-            return 0;            return 0;
-        }        }
-
+class Solution {class Solution {
+public:public:
+    int trap(vector<int>& height) {    int trap(vector<int>& height) {
         int l = 0;        int l = 0;
-        int r = n - 1;        int r = n - 1;
 
+        int total = 0;        int total = 0;
         while (l < r) {        while (l < r) {
-        int sum = 0;        int sum = 0;
-            leftMax = max(leftMax, height[l]);            leftMax = max(leftMax, height[l]);
-        int leftMax = 0;        int leftMax = 0;
-        int rightMax = 0;        int rightMax = 0;
-            rightMax = max(rightMax, height[r]);            rightMax = max(rightMax, height[r]);
-
-            if (leftMax < rightMax) {            if (leftMax < rightMax) {
-                sum += leftMax - height[l];                sum += leftMax - height[l];
-                l++;                l++;
-            } else {            } else {
-                sum += rightMax - height[r];                sum += rightMax - height[r];
-                r--;                r--;
-            }            }
         }        }
-            cout << sum << endl;            cout << sum << endl;
-        return sum;        return sum;
-    }    }
+            if (maxLeft <= maxRight) {            if (maxLeft <= maxRight) {
+            } else {            } else {
+                total += maxLeft - height[l];                total += maxLeft - height[l];
+                l++;                l++;
+                total += maxRight - height[r];                total += maxRight - height[r];
+            }            }
+                r--;                r--;
+
+        int maxLeft = height[l];        int maxLeft = height[l];
+        int r = height.size() - 1;        int r = height.size() - 1;
+        int maxRight = height[r];        int maxRight = height[r];
+            maxLeft = max(maxLeft, height[l]);            maxLeft = max(maxLeft, height[l]);
+            maxRight = max(maxRight, height[r]);            maxRight = max(maxRight, height[r]);
