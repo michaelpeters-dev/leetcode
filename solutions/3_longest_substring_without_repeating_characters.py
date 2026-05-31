931894ef3585cd3f3436c556ee56fd3477756ca1
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-31 18:48:04 -0400

    3. Longest Substring Without Repeating Characters (Python)

--- solutions/3_longest_substring_without_repeating_characters.py
+++ solutions/3_longest_substring_without_repeating_characters.py
@@ -3,29 +3,32 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Submission Status: Accepted
-# Runtime: 22 ms
-# Memory: 14.25 MB
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
 class Solution {class Solution {
 public:public:
     int lengthOfLongestSubstring(string s) {    int lengthOfLongestSubstring(string s) {
-        int n = s.size();        int n = s.size();
-        int l = 0;        int l = 0;
-        int r = 0;        int r = 0;
-        unordered_set<int> store;        unordered_set<int> store;
+        unordered_map<char, int> counter;        unordered_map<char, int> counter;
+        unordered_set<char> present;        unordered_set<char> present;
 
-    }    }
-        while (r < n) {        while (r < n) {
-            while (store.count(s[r])) {            while (store.count(s[r])) {
-        }        }
+        int trail = 0;        int trail = 0;
         int longest = 0;        int longest = 0;
-                store.erase(s[l]);                store.erase(s[l]);
+
+        for (int i = 0; i < s.length(); i++) {        for (int i = 0; i < s.length(); i++) {
+    }    }
+            char temp = s[i];            char temp = s[i];
+
+            while (present.count(temp)) {            while (present.count(temp)) {
+                counter[s[temp]]--;                counter[s[temp]]--;
             }            }
-                l++;                l++;
+                present.erase(s[trail]);                present.erase(s[trail]);
+                trail++;                trail++;
+        }        }
 
-            store.insert(s[r]);            store.insert(s[r]);
-            longest = max(longest, r - l);            longest = max(longest, r - l);
-            r++;            r++;
+            present.insert(temp);            present.insert(temp);
+            counter[temp]++;            counter[temp]++;
+            longest = max(longest, i - trail + 1);            longest = max(longest, i - trail + 1);
 
-        return longest + 1;        return longest + 1;
+        return longest;        return longest;
 };};
