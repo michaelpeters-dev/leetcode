24a8ee0603356d39567299b99b7646ccf7653d17
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-30 21:11:28 -0400

    128. Longest Consecutive Sequence (Python)

--- solutions/128_longest_consecutive_sequence.py
+++ solutions/128_longest_consecutive_sequence.py
@@ -3,31 +3,23 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/longest-consecutive-sequence/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: 0.00 MB
+# Runtime: 79 ms
+# Memory: 88.90 MB
 
-class Solution(object):class Solution(object):
-    def longestConsecutive(self, nums):    def longestConsecutive(self, nums):
-        """        """
-        :type nums: List[int]        :type nums: List[int]
-        :rtype: int        :rtype: int
-        """        """
+        }        }
 
-        # Add them all to a set        # Add them all to a set
-        # Then iterate through, if we don't have a number - 1 in the set then that's where we count from        # Then iterate through, if we don't have a number - 1 in the set then that's where we count from
+        int longest = 0;        int longest = 0;
+        for (const auto& num: store) {        for (const auto& num: store) {
+            if (!store.count(num - 1)) {            if (!store.count(num - 1)) {
+                int temp = num;                int temp = num;
+                int counter = 1;                int counter = 1;
+                while(store.count(temp)) {                while(store.count(temp)) {
+                    longest = max(longest, counter);                    longest = max(longest, counter);
+                    temp++;                    temp++;
+                    counter++;                    counter++;
+                }                }
+            }            }
+        }        }
 
-        store = set()        store = set()
-        for num in nums:        for num in nums:
-            store.add(num)            store.add(num)
-
-        for num in store:        for num in store:
-        longest = 0        longest = 0
-            if (num - 1) not in store:            if (num - 1) not in store:
-                counter = 1                counter = 1
-                while (num + 1) in store:                while (num + 1) in store:
-                    counter += 1                    counter += 1
-                    num += 1                    num += 1
-                longest = max(longest, counter)                longest = max(longest, counter)
-            else:            else:
-                continue                continue
-        return longest        return longest
+        return longest;        return longest;
+    }    }
