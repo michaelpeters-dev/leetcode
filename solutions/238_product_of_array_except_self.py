4c82154cc769c04eeeed5de0f0a80f7dcebb299a
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-05-30 20:44:09 -0400

    238. Product of Array Except Self (Python)

--- solutions/238_product_of_array_except_self.py
+++ solutions/238_product_of_array_except_self.py
@@ -3,26 +3,20 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/product-of-array-except-self/
 # Submission Status: Accepted
-# Runtime: 32 ms
-# Memory: 20.19 MB
+# Runtime: 0 ms
+# Memory: 40.12 MB
 
-class Solution(object):class Solution(object):
-    def productExceptSelf(self, nums):    def productExceptSelf(self, nums):
-        """        """
-        :type nums: List[int]        :type nums: List[int]
-        :rtype: List[int]        :rtype: List[int]
-        """        """
-        n = len(nums)        n = len(nums)
-        result = [1] * n        result = [1] * n
+        int suffix = 1;        int suffix = 1;
 
-        prefix = 1        prefix = 1
-        for i in range(n):        for i in range(n):
-            result[i] = prefix            result[i] = prefix
-            prefix *= nums[i]            prefix *= nums[i]
-                
-        suffix = 1        suffix = 1
-        for i in range(n-1, -1, -1):        for i in range(n-1, -1, -1):
-            result[i] *= suffix            result[i] *= suffix
-            suffix *= nums[i]            suffix *= nums[i]
-        return result        return result
-                
+        for (int i = nums.size() - 1; i>=0; i--) {        for (int i = nums.size() - 1; i>=0; i--) {
+            answer[i] *= suffix;            answer[i] *= suffix;
+            suffix *= nums[i];            suffix *= nums[i];
+        }        }
+        return answer;        return answer;
+    }    }
+
+        }        }
+            prefix *= nums[i];            prefix *= nums[i];
+            answer[i] *= prefix;            answer[i] *= prefix;
+        for (int i = 0; i < nums.size(); i++) {        for (int i = 0; i < nums.size(); i++) {
+};};
