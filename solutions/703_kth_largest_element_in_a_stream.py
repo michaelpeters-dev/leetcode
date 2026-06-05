30f9ae2a31fd700fc6e3e038fd1af3dab990fad7
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-04 23:12:35 -0400

    703. Kth Largest Element in a Stream (Python)

--- solutions/703_kth_largest_element_in_a_stream.py
+++ solutions/703_kth_largest_element_in_a_stream.py
@@ -3,21 +3,35 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/kth-largest-element-in-a-stream/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: N/A
+# Runtime: 17 ms
+# Memory: 33.08 MB
 
-class KthLargest:class KthLargest:
+class KthLargest {class KthLargest {
+public:public:
+    }    }
+        this->k = k;            this->k = k;    
+    KthLargest(int k, vector<int>& nums) {    KthLargest(int k, vector<int>& nums) {
+        
+    int add(int val) {    int add(int val) {
+    }    }
+        pq.push(val);        pq.push(val);
+};};
 
-    def __init__(self, k: int, nums: List[int]):    def __init__(self, k: int, nums: List[int]):
-        self.k = k        self.k = k
+/**/**
+ * Your KthLargest object will be instantiated and called as such: * Your KthLargest object will be instantiated and called as such:
+    int k;    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;    priority_queue<int, vector<int>, greater<int>> pq;
 
-    def add(self, val: int) -> int:    def add(self, val: int) -> int:
-        self.nums.append(val)        self.nums.append(val)
 
+        for (int num: nums) {        for (int num: nums) {
+            pq.push(num);            pq.push(num);
+        }        }
 
-# Your KthLargest object will be instantiated and called as such:# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)# obj = KthLargest(k, nums)
-        self.nums = nums        self.nums = nums
-        self.nums.sort(reverse=True)        self.nums.sort(reverse=True)
-        return self.nums[self.k-1]        return self.nums[self.k-1]
-# param_1 = obj.add(val)# param_1 = obj.add(val)
+            if (pq.size() > k) {            if (pq.size() > k) {
+                pq.pop();                pq.pop();
+            }            }
+        if (pq.size() > k) {        if (pq.size() > k) {
+            pq.pop();            pq.pop();
+        }        }
+
+        return pq.top();        return pq.top();
