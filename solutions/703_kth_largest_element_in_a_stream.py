5635607e121de5e38937ae102d5f85597caa9b35
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-05 11:06:13 -0400

    703. Kth Largest Element in a Stream (Python)

--- solutions/703_kth_largest_element_in_a_stream.py
+++ solutions/703_kth_largest_element_in_a_stream.py
@@ -3,35 +3,35 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/kth-largest-element-in-a-stream/
 # Submission Status: Accepted
-# Runtime: 17 ms
-# Memory: 33.08 MB
+# Runtime: 6 ms
+# Memory: 0.00 MB
 
 class KthLargest {class KthLargest {
 public:public:
-    }    }
-        this->k = k;            this->k = k;    
     KthLargest(int k, vector<int>& nums) {    KthLargest(int k, vector<int>& nums) {
+        this->k = k;        this->k = k;
+    }    }
         
     int add(int val) {    int add(int val) {
-    }    }
         pq.push(val);        pq.push(val);
+    }    }
 };};
 
 /**/**
  * Your KthLargest object will be instantiated and called as such: * Your KthLargest object will be instantiated and called as such:
-    int k;    int k;
     priority_queue<int, vector<int>, greater<int>> pq;    priority_queue<int, vector<int>, greater<int>> pq;
-
-
-        for (int num: nums) {        for (int num: nums) {
+    int k;    int k;
+        for (const auto& num: nums) {        for (const auto& num: nums) {
             pq.push(num);            pq.push(num);
         }        }
-
             if (pq.size() > k) {            if (pq.size() > k) {
                 pq.pop();                pq.pop();
             }            }
         if (pq.size() > k) {        if (pq.size() > k) {
             pq.pop();            pq.pop();
         }        }
-
         return pq.top();        return pq.top();
+ * KthLargest* obj = new KthLargest(k, nums); * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val); * int param_1 = obj->add(val);
+
+ */ */
