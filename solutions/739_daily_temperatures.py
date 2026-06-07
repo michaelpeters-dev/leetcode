5d9efbb9cefcd6b25b899123cd71fba0ef81f454
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-07 09:19:35 -0400

    739. Daily Temperatures (Python)

--- solutions/739_daily_temperatures.py
+++ solutions/739_daily_temperatures.py
@@ -3,17 +3,30 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/daily-temperatures/
 # Submission Status: Accepted
-# Runtime: 0 ms
-# Memory: N/A
+# Runtime: 28 ms
+# Memory: 107.36 MB
 
-class Solution:class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []        stack = []
-        res = [0] * len(temperatures)        res = [0] * len(temperatures)
+        // Specifically the decreasing property!        // Specifically the decreasing property!
 
-        for i in range(len(temperatures)):        for i in range(len(temperatures)):
-            while stack and temperatures[i]>temperatures[stack[-1]]:            while stack and temperatures[i]>temperatures[stack[-1]]:
-                prev = stack.pop()                prev = stack.pop()
-                res[prev] = i - prev                res[prev] = i - prev
-            stack.append(i)            stack.append(i)
-        return res        return res
+        stack<int> st;        stack<int> st;
+        // 75 72        // 75 72
+        for (int i = 0; i < temperatures.size(); i++) {        for (int i = 0; i < temperatures.size(); i++) {
+            if (st.size() == 0 || temperatures[st.top()] > temperatures[i]) {            if (st.size() == 0 || temperatures[st.top()] > temperatures[i]) {
+                st.push(i);                st.push(i);
+            }            }
+        }        }
+                continue;                continue;
+
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+
+        vector<int> result(temperatures.size());         vector<int> result(temperatures.size()); 
+                int index = st.top(); st.pop();                int index = st.top(); st.pop();
+                result[index] = i - index;                result[index] = i - index;
+            }            }
+            st.push(i);            st.push(i);
+
+        // Note: We need to keep the monotonic property of the stack intact        // Note: We need to keep the monotonic property of the stack intact
+    vector<int> dailyTemperatures(vector<int>& temperatures) {    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        return result;        return result;
+    }    }
+};};
