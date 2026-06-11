fcba3dc55cfcdf1bd37bc958d64decdecff24b00
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-10 22:27:34 -0400

    704. Binary Search (Python)

--- solutions/704_binary_search.py
+++ solutions/704_binary_search.py
@@ -3,20 +3,26 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/binary-search/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution(object):
-    def search(self, nums, target):
-        l = 0
-        r = len(nums)-1
+class Solution {class Solution {
+public:public:
+    int search(vector<int>& nums, int target) {    int search(vector<int>& nums, int target) {
+        int l = 0;        int l = 0;
+        int r = nums.size() - 1;        int r = nums.size() - 1;
 
-        while l<=r:
-            mid = (l + r)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[mid]>=target:
-                r = mid - 1
-            else:
-                l = mid + 1
-        return -1
+        while (l <= r) {        while (l <= r) {
+            int mid = (l + r) / 2;            int mid = (l + r) / 2;
+        }        }
+            if (nums[mid] == target) {            if (nums[mid] == target) {
+                return mid;                return mid;
+            } else if (nums[mid] < target) {            } else if (nums[mid] < target) {
+                l = mid + 1;                l = mid + 1;
+            } else {            } else {
+                r = mid - ;                r = mid - ;
+            }            }
+    }    }
+
+        return -1;        return -1;
+};};
