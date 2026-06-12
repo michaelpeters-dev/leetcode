6bfea90469354ccea11b5c6dbde3f776e2fdf8bd
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-11 21:50:53 -0400

    33. Search in Rotated Sorted Array (Python)

--- solutions/33_search_in_rotated_sorted_array.py
+++ solutions/33_search_in_rotated_sorted_array.py
@@ -3,35 +3,32 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/search-in-rotated-sorted-array/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 15.13 MB
 
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        l, r = 0, len(nums)-1
 
-        while l<=r:
-            mid = (l+r)//2
-            if target==nums[mid]:
-                return mid
+        int pivot = l;        int pivot = l;
+                
+        l = 0;        l = 0;
+        r = nums.size() - 1;        r = nums.size() - 1;
+        while (l <= r) {        while (l <= r) {
+            if (value < target) {            if (value < target) {
+                l = fakeMid + 1;                l = fakeMid + 1;
+        }        }
+                r = mid;                r = mid;
+            }            }
+            } else {            } else {
+            if (nums[mid] > nums[r]) {             if (nums[mid] > nums[r]) { 
+                l = mid + 1;                l = mid + 1;
+            int mid = (l + r) / 2;            int mid = (l + r) / 2;
+            int value = nums[realMid];            int value = nums[realMid];
+            int fakeMid = (l + r)/2;            int fakeMid = (l + r)/2;
+            int realMid = (fakeMid + pivot) % nums.size();            int realMid = (fakeMid + pivot) % nums.size();
+            } else if (value > target) {            } else if (value > target) {
+                r = fakeMid - 1;                r = fakeMid - 1;
+            } else {            } else {
+                return realMid;                return realMid;
+            }            }
+        }        }
 
-            if nums[mid]>=nums[l]:
-                if target>nums[mid]:
-                    l = mid + 1
-                elif target<nums[l]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            else:
-                if target>nums[r]:
-                    r = mid - 1
-                elif target<nums[mid]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-        return -1
+        return -1;        return -1;
