b7944d5995a133eca26474f85f68cdc78c8de0b6
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-13 22:21:09 -0400

    4. Median of Two Sorted Arrays (Python)

--- solutions/4_median_of_two_sorted_arrays.py
+++ solutions/4_median_of_two_sorted_arrays.py
@@ -4,21 +4,35 @@
 # URL: https://leetcode.com/problems/median-of-two-sorted-arrays/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 17.94 MB
+# Memory: 0.00 MB
 
-            ALeft = A[i] if i>=0 else float("-inf")            ALeft = A[i] if i>=0 else float("-inf")
-            ARight = A[i + 1] if i + 1 < len(A) else float("inf")            ARight = A[i + 1] if i + 1 < len(A) else float("inf")
-            BLeft = B[j] if j>=0 else float("-inf")            BLeft = B[j] if j>=0 else float("-inf")
-            BRight = B[j + 1] if j + 1 < len(B) else float("inf")            BRight = B[j + 1] if j + 1 < len(B) else float("inf")
+        int l = 0;        int l = 0;
+        int r = smaller->size();        int r = smaller->size();
+        while (l <= r) {        while (l <= r) {
+            int cut1 = (l + r) / 2;            int cut1 = (l + r) / 2;
+            int cut2 = ((m + n + 1) / 2) - cut1; // half the t-array minus the left partition            int cut2 = ((m + n + 1) / 2) - cut1; // half the t-array minus the left partition
 
-            if ALeft <= BRight and BLeft <= ARight:            if ALeft <= BRight and BLeft <= ARight:
-                if total%2==0:                if total%2==0:
-                    return (max(ALeft, BLeft) + min(ARight, BRight)) / 2                    return (max(ALeft, BLeft) + min(ARight, BRight)) / 2
-            # Calculate the lower and upper bounds of each array            # Calculate the lower and upper bounds of each array
+            int left1 = (cut1 == 0) ? INT_MIN : (*smaller)[cut1 - 1];            int left1 = (cut1 == 0) ? INT_MIN : (*smaller)[cut1 - 1];
+            int right1 = (cut1 == m) ? INT_MAX : (*smaller)[cut1];            int right1 = (cut1 == m) ? INT_MAX : (*smaller)[cut1];
 
-                return min(ARight, BRight)                return min(ARight, BRight)
-            elif ALeft > BRight:            elif ALeft > BRight:
-                r -= 1                r -= 1
-            else:            else:
-                l += 1                l += 1
-                        
+            int left2 = (cut2 == 0) ? INT_MIN : (*larger)[cut2 - 1];            int left2 = (cut2 == 0) ? INT_MIN : (*larger)[cut2 - 1];
+            int right2 = (cut2 == n) ? INT_MAX : (*larger)[cut2];            int right2 = (cut2 == n) ? INT_MAX : (*larger)[cut2];
+
+            if (left1 > right2) {            if (left1 > right2) {
+                r = cut1 - 1;                r = cut1 - 1;
+            } else if (left2 > right1) {            } else if (left2 > right1) {
+                l = cut1 + 1;                l = cut1 + 1;
+            } else {            } else {
+                int leftSide = max(left1, left2);                int leftSide = max(left1, left2);
+                if ((m + n)% 2 == 1) {                if ((m + n)% 2 == 1) {
+                    return leftSide;                    return leftSide;
+                }                }
+
+                int rightSide = min(right1, right2);                int rightSide = min(right1, right2);
+                return static_cast<double>(leftSide + rightSide)/2;                return static_cast<double>(leftSide + rightSide)/2;
+            }            }
+        }        }
+
+        return 0;        return 0;
+    }    }
+};};
