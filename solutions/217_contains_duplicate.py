053af8614917cf7a7da0d01b10be223076fbd7d5
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-25 20:31:36 -0400

    217. Contains Duplicate (Python)

--- solutions/217_contains_duplicate.py
+++ solutions/217_contains_duplicate.py
@@ -4,20 +4,22 @@
 # URL: https://leetcode.com/problems/contains-duplicate/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: 0.00 MB
+# Memory: N/A
 
 class Solution {class Solution {
 public:public:
     bool containsDuplicate(vector<int>& nums) {    bool containsDuplicate(vector<int>& nums) {
-        for (auto& num: nums) {        for (auto& num: nums) {
-        }        }
+        bool flag = false;        bool flag = false;
 
-        return false;        return false;
-        unordered_set<int> store;        unordered_set<int> store;
-            if (store.count(num)) {            if (store.count(num)) {
+        for (const auto& num: nums) {        for (const auto& num: nums) {
+        unordered_set<int> seen;        unordered_set<int> seen;
+            if (seen.count(num)) {            if (seen.count(num)) {
                 return true;                return true;
             } else {            } else {
-                store.insert(num);                store.insert(num);
+        }        }
+                seen.insert(num);                seen.insert(num);
             }            }
     }    }
+
+        return false;        return false;
 };};
