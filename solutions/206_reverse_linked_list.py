6eeedf67b8a3f91d81b4ce9f85aa4f5c379706c5
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-25 20:54:43 -0400

    206. Reverse Linked List (Python)

--- solutions/206_reverse_linked_list.py
+++ solutions/206_reverse_linked_list.py
@@ -3,20 +3,33 @@
 # Difficulty: Easy
 # URL: https://leetcode.com/problems/reverse-linked-list/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        curr = head
-        prev = None
-        while curr:
-            store = curr.next
-            curr.next = prev
-            prev = curr
-            curr = store
-        return prev
+/**/**
+ * Definition for singly-linked list. * Definition for singly-linked list.
+ * struct ListNode { * struct ListNode {
+ *     int val; *     int val;
+ *     ListNode *next; *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {} *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {} *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {} *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * }; * };
+ */ */
+class Solution {class Solution {
+public:public:
+    ListNode* reverseList(ListNode* head) {    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;        ListNode* prev = nullptr;
+        ListNode* curr = head;        ListNode* curr = head;
+
+        while (curr != nullptr) {        while (curr != nullptr) {
+            ListNode* next = curr->next;            ListNode* next = curr->next;
+        }        }
+            curr->next = prev;            curr->next = prev;
+
+    }    }
+            curr = next;            curr = next;
+            prev = curr;            prev = curr;
+
+        return prev;        return prev;
+};};
