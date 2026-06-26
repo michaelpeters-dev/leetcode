5bd5d0be18cfd0d672f30991f2d4a9666ad2d787
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-06-25 21:29:09 -0400

    143. Reorder List (Python)

--- solutions/143_reorder_list.py
+++ solutions/143_reorder_list.py
@@ -3,31 +3,35 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/reorder-list/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution(object):
-    def reorderList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: None Do not return anything, modify head in-place instead.
-        """
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        ListNode* second = slow->next;        ListNode* second = slow->next;
+        slow->next = nullptr;        slow->next = nullptr;
 
-        second = slow.next
-        prev = slow.next = None
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
+        ListNode* prev = nullptr;        ListNode* prev = nullptr;
+        while (second != nullptr) {        while (second != nullptr) {
+            ListNode* next = second->next;            ListNode* next = second->next;
 
-        first, second = head, prev
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
+            second->next = prev;            second->next = prev;
+            prev = second;            prev = second;
+            second = next;            second = next;
+        }        }
+
+        }        }
+            fast = fast->next->next;            fast = fast->next->next;
+            slow = slow->next;            slow = slow->next;
+        while (fast->next != nullptr && fast->next->next != nullptr) {        while (fast->next != nullptr && fast->next->next != nullptr) {
+
+        ListNode* fast = head;        ListNode* fast = head;
+        ListNode* slow = head;        ListNode* slow = head;
+        }        }
+
+    void reorderList(ListNode* head) {    void reorderList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {        if (head == nullptr || head->next == nullptr) {
+            return;            return;
+class Solution {class Solution {
+public:public:
+ */ */
+ * }; * };
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {} *     ListNode(int x, ListNode *next) : val(x), next(next) {}
