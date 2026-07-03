e184adf012f884be84a6b78abf1bcb3967e1f24b
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-07-03 19:09:35 -0400

    235. Lowest Common Ancestor of a Binary Search Tree (Python)

--- solutions/235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ solutions/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -3,18 +3,26 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return None
+ */ */
 
-        if p.val < root.val and q.val<root.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+class Solution {class Solution {
+public:public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    // We want continue moving downward until we have: p < root < q    // We want continue moving downward until we have: p < root < q
+        if (p->val > root->val && q->val > root->val) {        if (p->val > root->val && q->val > root->val) {
+        } else if (p->val < root->val && q->val < root->val) {        } else if (p->val < root->val && q->val < root->val) {
+            return lowestCommonAncestor(root->left, p, q);            return lowestCommonAncestor(root->left, p, q);
+        } else {        } else {
+        }        }
+        if (p->val > q->val) {        if (p->val > q->val) {
+            swap(p, q);            swap(p, q);
+        }        }
 
-        elif p.val > root.val and q.val > root.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        else:
-            return root
+    }    }
+ * }; * };
+            return lowestCommonAncestor(root->right, p, q);            return lowestCommonAncestor(root->right, p, q);
+            return root;            return root;
+};};
