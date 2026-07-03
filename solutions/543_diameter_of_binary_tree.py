071293791ab7085719b65e32cd364ef21cdf72e3
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-07-03 14:47:50 -0400

    543. Diameter of Binary Tree (Python)

--- solutions/543_diameter_of_binary_tree.py
+++ solutions/543_diameter_of_binary_tree.py
@@ -4,22 +4,31 @@
 # URL: https://leetcode.com/problems/diameter-of-binary-tree/
 # Submission Status: Accepted
 # Runtime: 0 ms
-# Memory: N/A
+# Memory: 0.00 MB
 
-#         self.val = val#         self.val = val
-#         self.left = left#         self.left = left
-#         self.right = right#         self.right = right
-class Solution:class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        maximum = [0]        maximum = [0]
-        def dfs(root):        def dfs(root):
-            if not root:            if not root:
-                return 0                return 0
-            max_left_branch = dfs(root.left)            max_left_branch = dfs(root.left)
-            max_right_branch = dfs(root.right)            max_right_branch = dfs(root.right)
-                        
-            curr_diameter = max_left_branch + max_right_branch            curr_diameter = max_left_branch + max_right_branch
-            maximum[0] = max(maximum[0], curr_diameter)            maximum[0] = max(maximum[0], curr_diameter)
-            return 1 + max(max_left_branch, max_right_branch)            return 1 + max(max_left_branch, max_right_branch)
-        dfs(root)        dfs(root)
-        return maximum[0]        return maximum[0]
+public:public:
+    int diameterOfBinaryTree(TreeNode* root) {    int diameterOfBinaryTree(TreeNode* root) {
+    int findMax(TreeNode* node) {    int findMax(TreeNode* node) {
+
+        if (node == nullptr) {        if (node == nullptr) {
+    }    }
+            return 0;            return 0;
+        }        }
+
+        return max(findMax(node->left), findMax(node->right)) + 1;        return max(findMax(node->left), findMax(node->right)) + 1;
+        if (root->left != nullptr && root->right != nullptr) {        if (root->left != nullptr && root->right != nullptr) {
+        int maximum = 0;        int maximum = 0;
+        } else if (root -> left != nullptr) {        } else if (root -> left != nullptr) {
+            maximum = findMax(root->left) + findMax(root->right);            maximum = findMax(root->left) + findMax(root->right);
+            maximum = findMax(root->left);            maximum = findMax(root->left);
+        } else if (root -> right != nullptr) {        } else if (root -> right != nullptr) {
+            maximum = findMax(root->right);            maximum = findMax(root->right);
+        }        }
+    }    }
+            return max(max(diameterOfBinaryTree(root->left), diameterOfBinaryTree(root->right)), maximum);            return max(max(diameterOfBinaryTree(root->left), diameterOfBinaryTree(root->right)), maximum);
+            return max(maximum, diameterOfBinaryTree(root->left));            return max(maximum, diameterOfBinaryTree(root->left));
+            return max(maximum, diameterOfBinaryTree(root->right));            return max(maximum, diameterOfBinaryTree(root->right));
+class Solution {class Solution {
+ */ */
+
+        return maximum;        return maximum;
