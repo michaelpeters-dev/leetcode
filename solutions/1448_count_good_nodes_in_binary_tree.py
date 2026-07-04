020c099ee22f803e13aa2eef9326cc1994149e1a
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-07-03 23:26:27 -0400

    1448. Count Good Nodes in Binary Tree (Python)

--- solutions/1448_count_good_nodes_in_binary_tree.py
+++ solutions/1448_count_good_nodes_in_binary_tree.py
@@ -3,23 +3,29 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 # Submission Status: Accepted
-# Runtime: 48 ms
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-# class TreeNode:# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val#         self.val = val
-#         self.left = left#         self.left = left
-#         self.right = right#         self.right = right
-class Solution:class Solution:
-    def goodNodes(self, root: TreeNode) -> int:    def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, maxVal):        def dfs(node, maxVal):
-            if not node:            if not node:
-                return 0                return 0
-            res = 1 if node.val >= maxVal else 0            res = 1 if node.val >= maxVal else 0
-            maxVal = max(maxVal, node.val)            maxVal = max(maxVal, node.val)
-            res += dfs(node.left, maxVal)            res += dfs(node.left, maxVal)
-            res += dfs(node.right, maxVal)            res += dfs(node.right, maxVal)
-            return res            return res
-        return dfs(root, root.val)        return dfs(root, root.val)
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * }; * };
+ */ */
+class Solution {class Solution {
+public:public:
+    int dfs(TreeNode* node, int msf) {    int dfs(TreeNode* node, int msf) {
+    }    }
+        if (node == nullptr) {        if (node == nullptr) {
+            return 0;            return 0;
+        }        }
 
+        if (node->val >= msf) {        if (node->val >= msf) {
+        }        }
+
+    int goodNodes(TreeNode* root) {    int goodNodes(TreeNode* root) {
+        int good = 0;        int good = 0;
+            good++;            good++;
+        msf = max(node->val, msf);        msf = max(node->val, msf);
+
+        return good + dfs(node->left, msf) + dfs(node->right, msf);        return good + dfs(node->left, msf) + dfs(node->right, msf);
+    }    }
+        return dfs(root, root->val);        return dfs(root, root->val);
+};};
