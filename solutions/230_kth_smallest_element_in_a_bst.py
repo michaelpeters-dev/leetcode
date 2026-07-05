b2c2e1d616f75a0bafe06a4381052a4a7a22e48d
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-07-05 19:29:46 -0400

    230. Kth Smallest Element in a BST (Python)

--- solutions/230_kth_smallest_element_in_a_bst.py
+++ solutions/230_kth_smallest_element_in_a_bst.py
@@ -3,19 +3,29 @@
 # Difficulty: Medium
 # URL: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        cur, stack = root, []
-        n = 0
-        while cur or stack:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-            cur = stack.pop()
-            n += 1
-            if n==k:
-                return cur.val
-            cur = cur.right
+        if (counter == k) {        if (counter == k) {
+            dummy->val = curr->val;            dummy->val = curr->val;
+        inOrder(curr->left, dummy, k);        inOrder(curr->left, dummy, k);
+
+        }        }
+            return nullptr;            return nullptr;
+        if (curr == nullptr) {        if (curr == nullptr) {
+    TreeNode* inOrder(TreeNode* curr, TreeNode* dummy, int k) {    TreeNode* inOrder(TreeNode* curr, TreeNode* dummy, int k) {
+    static inline int counter;    static inline int counter;
+public:public:
+class Solution {class Solution {
+        counter++;        counter++;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {} *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * }; * };
+ */ */
+ *     TreeNode *left; *     TreeNode *left;
+ *     TreeNode *right; *     TreeNode *right;
+ * struct TreeNode { * struct TreeNode {
+ *     int val; *     int val;
+ * Definition for a binary tree node. * Definition for a binary tree node.
+/**/**
