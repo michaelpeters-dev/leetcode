a2661b1558a659d778ab034f390caa08e6547707
Michael Peters <123867234+michaelpeters-dev@users.noreply.github.com>
2026-07-06 21:23:57 -0400

    124. Binary Tree Maximum Path Sum (Python)

--- solutions/124_binary_tree_maximum_path_sum.py
+++ solutions/124_binary_tree_maximum_path_sum.py
@@ -3,26 +3,29 @@
 # Difficulty: Hard
 # URL: https://leetcode.com/problems/binary-tree-maximum-path-sum/
 # Submission Status: Accepted
-# Runtime: N/A
-# Memory: N/A
+# Runtime: 0 ms
+# Memory: 0.00 MB
 
-class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = [root.val]
+        if (node == nullptr) {        if (node == nullptr) {
+        }        }
+            return 0;            return 0;
 
-        def dfs(root):
-            if not root:
-                return 0
+        int leftMax = maximum(node->left);        int leftMax = maximum(node->left);
+        int rightMax = maximum(node->right);        int rightMax = maximum(node->right);
+                
+        largest = max(largest, leftMax + rightMax + node->val);        largest = max(largest, leftMax + rightMax + node->val);
+        largest = max(largest, leftMax + node->val);        largest = max(largest, leftMax + node->val);
+        largest = max(largest, rightMax + node->val);        largest = max(largest, rightMax + node->val);
+    int maximum(TreeNode* node) {    int maximum(TreeNode* node) {
+    int largest;    int largest;
+        largest = max(largest, node->val);        largest = max(largest, node->val);
 
-            left_max = dfs(root.left)
-            right_max = dfs(root.right)
+        return max(node->val, max(node->val + leftMax, node->val + rightMax));        return max(node->val, max(node->val + leftMax, node->val + rightMax));
+    }    }
 
-            left_max = max(left_max, 0)
-            right_max = max(right_max, 0)
-
-            res[0] = max(res[0], root.val + left_max + right_max)
-
-            return root.val + max(left_max, right_max)
-
-        dfs(root)
-        return res[0]
+    int maxPathSum(TreeNode* root) {    int maxPathSum(TreeNode* root) {
+        largest = INT_MIN;        largest = INT_MIN;
+        maximum(root);        maximum(root);
+    }    }
+        return largest;        return largest;
+};};
